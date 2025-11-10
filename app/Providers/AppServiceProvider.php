63b1280c308bd8e827e105f1ba0638824bc3a06c
fasya putri aureli <fasyaputriaureli@fasyas-MacBook-Air.local>
2025-11-10 19:53:06 +0700

    fix: force https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,25 +2,20 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        // Paksa semua URL menjadi HTTPS saat di hosting (contoh Railway, Vercel, dll)
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
     }
